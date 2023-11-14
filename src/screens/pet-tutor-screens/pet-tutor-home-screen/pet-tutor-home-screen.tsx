91a03a7d52e48b38a23a94be688c4d859a6e03c5
Leonardo M. Primieri <leo.primieri@gmail.com>
2023-11-14 10:42:51 -0300

    Update image for "Mapa de Clínicas" menu item

--- src/screens/pet-tutor-screens/pet-tutor-home-screen/pet-tutor-home-screen.tsx
+++ src/screens/pet-tutor-screens/pet-tutor-home-screen/pet-tutor-home-screen.tsx
@@ -29,7 +29,7 @@ const MENU_ITEMS = [
   },
   {
     label: "Mapa de Clínicas",
-    image: require("~/assets/medical.png"),
+    image: require("~/assets/world.png"),
     path: "VeterinaryMap",
   },
 ];
